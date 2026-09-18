73723dba86ae20268ada44015bf1b9472912a183
kaifalam09 <mrkaifalam0311@gmail.com>
2026-09-19 02:40:17 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -3,10 +3,10 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:share_plus/share_plus.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await MobileAds.instance.initialize();
+  MobileAds.instance.initialize();
 
   runApp(const VibeRushApp());
 }
